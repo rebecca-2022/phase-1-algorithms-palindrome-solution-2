1c89c8feb22d001c1b97a11bbdb17a722679431c
rebecca-2022 <rebecca.njeri@student.moringaschool.com>
2022-08-27 16:12:32 +0300

    completed assignment

--- test/index.js
+++ test/index.js
@@ -1,21 +1,13 @@
-const chai = require("chai");
-global.expect = chai.expect;
-const isPalindrome = require("../index");
-
-describe("isPalindrome", () => {
-  it("returns true for 'abba'", () => {
-    expect(isPalindrome("abba")).to.be.true;
-  });
-  it("returns true for 'racecar'", () => {
-    expect(isPalindrome("racecar")).to.be.true;
-  });
-  it("returns true for 'a'", () => {
-    expect(isPalindrome("a")).to.be.true;
-  });
-  it("returns false for 'robot'", () => {
-    expect(isPalindrome("robot")).to.be.false;
-  });
-  it("returns false for 'ab'", () => {
-    expect(isPalindrome("ab")).to.be.false;
-  });
-});
+function isPalindrome(word) {
+  // Write your algorithm here
+  //iterate through the word to the middle
+  for (let i = 0; i < word.length / 2; i++) {
+    //compare the letters with the corresponding characters
+    let j = word.length - i - 1;
+    // if the letters are different, then return false
+    if (word[i] !== word[j]) {
+      return false;
+    }
+  }
+  return true;
+  }
