f41ee049c1c3163a165efbfcc867e5749b4bf4e7
rebecca-2022 <rebecca.njeri@student.moringaschool.com>
2022-08-29 11:40:34 +0300

    completed assignment

--- test/index.js
+++ test/index.js
@@ -1,13 +1,51 @@
 function isPalindrome(word) {
   // Write your algorithm here
-  //iterate through the word to the middle
+  //iterate from the begining to the middle
   for (let i = 0; i < word.length / 2; i++) {
-    //compare the letters with the corresponding characters
-    let j = word.length - i - 1;
-    // if the letters are different, then return false
-    if (word[i] !== word[j]) {
-      return false;
-    }
+
+    //check each letter to the corresponding letter from the end
+    const j = word.length -1 -i
+    const startChar = word[i]
+    const endChar  = word[j]
+
+    // if any letters don't match, return false
+    if (startChar !== endChar) return false;
+
+    //return true
+    return true;
+
   }
-  return true;
-  }
+  
+  
+  
+
+}
+
+/* 
+  Add your pseudocode here
+  That means if the 
+  first letter is the same as the last letter, and the second letter is the same as the second last letter,etc.
+  until we reach the middle,return true.
+  iterate from the begining to the middle
+     check each letter to the corresponding letter from the end
+     if any letters don't match, return false
+  return true
+*/
+
+/*
+  Add written explanation of your solution here
+*/
+
+// You can run `node index.js` to view these console logs
+if (require.main === module) {
+  // add your own custom tests in here
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("racecar"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("robot"));
+}
+
+module.exports = isPalindrome;
